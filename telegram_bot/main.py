2f448beef0f65dde42b197896f5d4bf9e21c7695
Mkrtich Mkrtchyan <mkrtich@bl.ink>
2024-10-11 13:27:01 +0400

    fix telebot issue

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -1,8 +1,10 @@
 import os
 from telegram import Bot
+from telegram.request import HTTPXRequest
 
+request = HTTPXRequest(pool_timeout=5)
 bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
-bot = Bot(token=bot_token)
+bot = Bot(token=bot_token,  request=request)
 
 
 async def send_message(chat_id, text, markup=None):
