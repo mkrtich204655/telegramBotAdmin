fcc9219cd6330801ae69ea28d6d8cfb56cc5c3b4
Mkrtich Mkrtchyan <mkrtich@bl.ink>
2024-10-22 16:45:57 +0400

    remove dashboard app

--- telegramBotAdmin/urls.py
+++ telegramBotAdmin/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from userauth import urls as auth_urls
-from dashboard import urls as dashboard_urls
 from users import urls as users_urls
+from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
-    path('', include(dashboard_urls)),
     path('login/', include(auth_urls)),
     path('tuser/', include(users_urls)),
 ]
