1acc8c4a1919df3ac1eab80bf778c47a3704f642
Mkrtich Mkrtchyan <mkrtich@bl.ink>
2024-11-15 14:41:49 +0400

    suggest existing cars

--- users/services/cars_service.py
+++ users/services/cars_service.py
@@ -11,3 +11,6 @@ class CarsService:
 
     def check_car_by_user_id_and_number(self, user_id, number):
         return self.model.objects.filter(user_id=user_id, number=number).first()
+
+    def get_cars_by_user_id(self, user_id):
+        return self.model.objects.filter(user_id=user_id).all()
--- users/views_folder/cars_views.py
+++ users/views_folder/cars_views.py
@@ -2,6 +2,8 @@ from django.http import JsonResponse
 from rest_framework.viewsets import ViewSet
 from users.serializers_folder.car_serializer import CarSerializer, CarCreateSerializer
 from django.views.decorators.csrf import csrf_exempt
+
+from users.serializers_folder.user_api_serializer import UserApiSerializer
 from users.services.cars_service import CarsService
 from users.services.user_service import UserService
 from telegram_bot.decode import decrypt_json
@@ -40,3 +42,23 @@ class CarView(ViewSet):
             return JsonResponse(self.encrypt(CarSerializer(car).data), safe=False, status=201)
         else:
             return JsonResponse(self.encrypt(car_serializer.errors), safe=False, status=400)
+
+
+    def get_cars(self, request):
+        decode_data = self.decrypt(json.loads(request.body))
+        user_id_serializer = UserApiSerializer(data=decode_data)
+
+        if user_id_serializer.is_valid():
+            user_data = user_id_serializer.validated_data
+
+            user = self.userService.get_user_by_TUID(user_data['tuid'])
+            if user is None:
+                return JsonResponse(self.encrypt({'error': 'User not found'}), status=404)
+
+            cars = self.service.get_cars_by_user_id(user.id)
+            if cars is None:
+                return JsonResponse(self.encrypt({'message': 'user does not any cars yet'}), status=404)
+
+            return JsonResponse(self.encrypt(CarSerializer(cars, many=True).data), safe=False, status=200)
+        else:
+            return JsonResponse(self.encrypt(user_id_serializer.errors), safe=False, status=400)
